dfb45635ea861c052e1e4cbcfea0ec0abb5e144b
Akshay <soniaks@sheridancollege.ca>
2026-05-26 23:17:12 -0400

    Initial portfolio commit with complete design

---
+++ Static/js/main.js
@@ -0,0 +1,14 @@
+// document.addEventListener("scroll", () => {
+//     document.querySelectorAll(".about, .skills, .project-card").forEach(el => {
+//         const position = el.getBoundingClientRect().top;
+//         const screen = window.innerHeight;
+
+//         if (position < screen - 100) {
+//             el.style.opacity = 1;
+//         }
+//     });
+// });
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Portfolio Loaded 🚀");
+});
